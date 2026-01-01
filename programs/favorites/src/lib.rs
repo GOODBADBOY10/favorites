d77301a31aef27d758d36bd08848dd59636a399a
Goodbadboy <selfmanademola@gmail.com>
2026-01-01 13:51:02 +0100

    Implement favorites functionality with initialization and struct definitions

--- programs/favorites/src/lib.rs
+++ programs/favorites/src/lib.rs
@@ -2,6 +2,8 @@ use anchor_lang::prelude::*;
 
 declare_id!("6HxogBU2AvwFmQLqyxQsJMaWuhaDoZ4Qx2czCYsuZbTw");
 
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
 #[program]
 pub mod favorites {
     use super::*;
@@ -10,6 +12,18 @@ pub mod favorites {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn set_favorites() -> Result<()> {}
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Favorite {
+    pub number: u64,
+    #[max_len(50)]
+    pub color: String,
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>,
 }
 
 #[derive(Accounts)]
